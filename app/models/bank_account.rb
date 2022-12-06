a8021374b6bf2aea49a06d1305c110b524cabc5d
anand <anand@webkorps.com>
2022-12-06 17:49:38 +0530

    add ifsc code

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,6 +1,12 @@
 class BankAccount < ApplicationRecord
 	belongs_to :user
 	before_create :active_account
+
+	validates :account_number,:presence => true,
+                 :numericality => true,
+                 :presence => true,
+                 :length => { :minimum => 10, :maximum => 18 }
+  validates :bank_name, :account_holder_name, presence: true             
 	
 	def deactive_account
 	  bank_details = User.find_by_id(self.user_id).bank_accounts.where.not(id: id).last
