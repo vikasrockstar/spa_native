27168412083a237799fd520df5d091364aa5e958
Abdul <abdul@webkorps.com>
2023-05-30 16:48:09 +0530

    changed currency

--- app/services/stripe_payment.rb
+++ app/services/stripe_payment.rb
@@ -29,13 +29,13 @@ class StripePayment
 
   def create_product_price(amount)
     Stripe::Price.create(
-      { currency: 'inr', unit_amount: amount, product: @product['id'] }
+      { currency: 'aed', unit_amount: amount, product: @product['id'] }
     )
   end
 
   def create_custom_price
     Stripe::Price.create(
-      { currency: 'inr', custom_unit_amount: {enabled: true}, product: @product['id'] }
+      { currency: 'aed', custom_unit_amount: {enabled: true}, product: @product['id'] }
     )
   end
   
