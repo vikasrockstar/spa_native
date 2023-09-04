9455f2beed4c3e82f6e8d49f90f0af205dbc9b8c
vikas <vikaskumar@webkorps.com>
2023-09-04 14:46:48 +0530

    Added new host.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
   config.hosts << /.*\.ngrok\.io/
-  config.hosts << "6a18-103-54-100-147.ngrok-free.app"
+  config.hosts << "admin.mytipsdubai.com"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
