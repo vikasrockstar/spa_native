a4ab103fcbaffb1e9629e418d78a7432b0192a25
vanshika2902 <vanshika@webkorps.com>
2023-09-21 19:17:34 +0530

    applied validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
-  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,13 +33,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "bank_accounts", charset: "utf8mb4", force: :cascade do |t|
+  create_table "bank_accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "bank_name"
     t.string "account_holder_name"
     t.string "account_number"
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.index ["deleted_at"], name: "index_bank_accounts_on_deleted_at"
   end
 
-  create_table "events", charset: "utf8mb4", force: :cascade do |t|
+  create_table "events", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.json "data"
     t.string "source"
     t.string "event_type"
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "qr_codes", charset: "utf8mb4", force: :cascade do |t|
+  create_table "qr_codes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "url"
     t.float "amount"
     t.bigint "user_id", null: false
@@ -78,7 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.index ["user_id"], name: "index_qr_codes_on_user_id"
   end
 
-  create_table "reviews", charset: "utf8mb4", force: :cascade do |t|
+  create_table "reviews", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "rating"
     t.text "review"
     t.bigint "user_id", null: false
@@ -87,7 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table "transactions", charset: "utf8mb4", force: :cascade do |t|
+  create_table "transactions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.float "amount"
     t.integer "bank_account_id"
     t.bigint "user_id", null: false
@@ -98,7 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.string "first_name"
@@ -124,7 +124,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.string "language_preference", default: "English"
   end
 
-  create_table "version_managers", charset: "utf8mb4", force: :cascade do |t|
+  create_table "version_managers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "device_type"
     t.string "app_version"
     t.string "message"
@@ -134,18 +134,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "wallet_histories", charset: "utf8mb4", force: :cascade do |t|
-    t.integer "updated_by_user"
-    t.float "previous_balance"
-    t.float "updated_balance"
-    t.string "checkout_session_object_id"
-    t.bigint "wallet_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["wallet_id"], name: "index_wallet_histories_on_wallet_id"
-  end
-
-  create_table "wallets", charset: "utf8mb4", force: :cascade do |t|
+  create_table "wallets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.float "balance", default: 0.0
     t.bigint "user_id"
     t.datetime "created_at", null: false
@@ -153,7 +142,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
 
-  create_table "withdrawal_requests", charset: "utf8mb4", force: :cascade do |t|
+  create_table "withdrawal_requests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.float "amount", default: 0.0
     t.integer "status", default: 0
     t.datetime "completion_date"
@@ -168,6 +157,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_081406) do
   add_foreign_key "qr_codes", "users"
   add_foreign_key "reviews", "users"
   add_foreign_key "transactions", "users"
-  add_foreign_key "wallet_histories", "wallets"
   add_foreign_key "withdrawal_requests", "users"
 end
